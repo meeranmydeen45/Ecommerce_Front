4f4e2a945f4749dc0c915a241cbbf983af1a1a9a
meeran <meeranmydeen@gmail.com>
2021-01-02 13:36:48 -0800

    Code Formatting

--- src/Components/NewProducts.js
+++ src/Components/NewProducts.js
@@ -4,12 +4,14 @@ const NewProducts = () => {
 
 return(
 <div className="newProducts">
-<div className="newProducts-Inner">    
-<div style={{textAlign:'center'}}><b>Register Your New Products</b></div>
-<input type="text" className="textBoxNewRegister"/>
-<input type="button" value="Save" className="buttonNewRegister"/>
-<input type="button" value="Show" className="buttonNewRegister"/>
-</div>
+  <div className="newProducts-Inner">    
+    <div style={{textAlign:'center'}}>
+      <b>Register Your New Products</b>
+    </div>
+    <input type="text" className="textBoxNewRegister"/>
+    <input type="button" value="Save" className="buttonNewRegister"/>
+    <input type="button" value="Show" className="buttonNewRegister"/>
+  </div>
 </div>
 
 )
