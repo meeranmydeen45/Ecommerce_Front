b2424f73f4ba2eb1bc642d732cbb3baaa8629ef6
Meeran <meeranmydeen@gmail.com>
2021-04-21 01:14:10 +0400

    Table-Creation

--- src/shared/utils/helper.js
+++ src/shared/utils/helper.js
@@ -25,6 +25,7 @@ export const getToken = () => {
 };
 
 export const designPDFwithData = async (data) => {
+  console.log(data);
   //Parent Div of all Content in PrintPage
   const div = document.createElement('div');
   div.setAttribute('id', 'divParent');
@@ -46,9 +47,80 @@ export const designPDFwithData = async (data) => {
   div.appendChild(divHeader);
   //#endregion Div Header Section
 
+  //#start - Div for Date and Invoice No
+  const div2Section = document.createElement('div');
+  div2Section.setAttribute('class', 'div2-Section');
+
+  const divForCust = document.createElement('div');
+
+  divForCust.setAttribute('class', 'div-Cust');
+  var labelId = document.createElement('label');
+  labelId.innerHTML = 'Customer Id:';
+  divForCust.appendChild(labelId);
+  var tagPforId = document.createElement('p');
+  tagPforId.style.display = 'inline-block';
+  tagPforId.innerHTML = data.custwithorder.customer.customerId;
+  divForCust.appendChild(tagPforId);
+  const breakLine = document.createElement('br');
+  divForCust.appendChild(breakLine);
+
+  var labelName = document.createElement('label');
+  labelName.innerHTML = 'Customer Name:';
+  divForCust.appendChild(labelName);
+  var tagPforName = document.createElement('p');
+  tagPforName.style.display = 'inline-block';
+  tagPforName.innerHTML = data.custwithorder.customer.customerName;
+  divForCust.appendChild(tagPforName);
+  const breakLine2 = document.createElement('br');
+  divForCust.appendChild(breakLine2);
+
+  var labelMobile = document.createElement('label');
+  labelMobile.style.display = 'inline';
+  labelMobile.innerHTML = 'Mobile Number:';
+  divForCust.appendChild(labelMobile);
+  var tagPforMobile = document.createElement('p');
+  tagPforMobile.style.display = 'inline-block';
+  tagPforMobile.innerHTML = data.custwithorder.customer.customermobile;
+  divForCust.appendChild(tagPforMobile);
+
+  div2Section.appendChild(divForCust);
+
+  // Date and Inovice No. Section
+  const divForDate = document.createElement('div');
+  divForDate.setAttribute('class', 'div-Date');
+
+  var labelDate = document.createElement('label');
+  labelDate.style.display = 'inline-block';
+  labelDate.innerHTML = 'Date :';
+  divForDate.appendChild(labelDate);
+  var tagPforDate = document.createElement('p');
+  var date = new Date();
+  const setDate = date.getDate() + '/' + parseInt(date.getMonth()) + 1 + '/' + date.getFullYear();
+
+  tagPforDate.innerHTML = setDate;
+  tagPforDate.style.display = 'inline-block';
+  divForDate.appendChild(tagPforDate);
+  const breakLine3 = document.createElement('br');
+  divForDate.appendChild(breakLine3);
+
+  var labelBillNo = document.createElement('label');
+  labelBillNo.innerHTML = 'Bill Number :';
+  divForDate.appendChild(labelBillNo);
+  var tagPforBillNo = document.createElement('p');
+  tagPforBillNo.style.display = 'inline-block';
+  tagPforBillNo.innerHTML = data.billnumber;
+  divForDate.appendChild(tagPforBillNo);
+
+  div2Section.appendChild(divForDate);
+
+  div.appendChild(div2Section);
+
+  //#endregion
+
   //starts - Div Table Section
   const divTable = document.createElement('div');
   divTable.setAttribute('id', 'divTable');
+  divTable.setAttribute('class', 'divTable');
   const table = document.createElement('table');
   var props = [];
   for (var propName in data.custwithorder.cartItems[0]) {
@@ -58,20 +130,27 @@ export const designPDFwithData = async (data) => {
   for (var i = 0; i < data.custwithorder.cartItems.length; i++) {
     var tr = document.createElement('tr');
     for (var j = 0; j < props.length; j++) {
-      var td = document.createElement('td');
-
-      var textNode = document.createTextNode(data.custwithorder.cartItems[i][props[j]]);
-
-      td.appendChild(textNode);
-      tr.appendChild(td);
+      if (j === 0) {
+        // To Generate Serial Number in Invoice
+        var td = document.createElement('td');
+        var textNode = document.createTextNode(i + 1);
+        td.appendChild(textNode);
+        tr.appendChild(td);
+      } else if (j !== 2) {
+        //To skip Image Name and Id propery
+        var td = document.createElement('td');
+        var textNode = document.createTextNode(data.custwithorder.cartItems[i][props[j]]);
+        td.appendChild(textNode);
+        tr.appendChild(td);
+      }
     }
     table.appendChild(tr);
   }
-  console.log(table);
+
   divTable.appendChild(table);
   div.appendChild(divTable);
   //#endregion of Table Section
-  console.log(div);
+
   return await div;
 };
 
